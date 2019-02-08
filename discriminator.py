2b044bba8e45a67e26980dfb6d86fe146ab050c9
dujiajun <dujiajun1994@163.com>
2019-02-08 17:06:14 +0800

    layer norm

--- discriminator.py
+++ discriminator.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import json
 
 
@@ -10,37 +9,42 @@ class Discriminator(nn.Module):
         with open('data/vocab.json') as fid:
             vocab = json.load(fid)
         self.vocab_size = len(vocab) + 1
-        self.word_embed = nn.Embedding(self.vocab_size, args.input_encoding_size)
-        self.lstm_embed1 = nn.Embedding(self.vocab_size, args.input_encoding_size)
-        self.lstm1 = nn.LSTM(args.input_encoding_size, args.rnn_size, num_layers=1, batch_first=True)
-        self.lstm_embed2 = nn.Embedding(self.vocab_size, args.input_encoding_size)
-        self.lstm2 = nn.LSTM(args.input_encoding_size, args.rnn_size, num_layers=1, batch_first=True)
-        self.output_layer = nn.Linear(args.rnn_size * 2 + args.input_encoding_size, 1)
+        hidden_size = 512
+        self.word_embed = nn.Embedding(self.vocab_size, hidden_size)
+        self.lstm_embed = nn.Embedding(self.vocab_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=1, batch_first=True)
+        self.output_layer = nn.Linear(hidden_size * 2, 1)
 
     def forward(self, seqs1, seqs2):
-        word_dot = self._word_embed(seqs1) * self._word_embed(seqs2)
-        lstm_dot = self._lstm_embed(seqs1) * self._lstm_embed(seqs2)
-        lstm_output = self._lstm_embed(seqs2, is_first=False)
-        outputs = torch.cat([word_dot, lstm_dot, lstm_output], 1)
-        outputs = torch.sigmoid(self.output_layer(outputs)).squeeze(1)
+        dot = self._embed(seqs1) * self._embed(seqs2)
+        outputs = torch.sigmoid(self.output_layer(dot)).squeeze(1)
         return outputs
+
+    def _embed(self, seqs):
+        word_embed = self._word_embed(seqs)
+        lstm_embed = self._lstm_embed(seqs)
+        embed = torch.cat([word_embed, lstm_embed], 1)
+        return embed
 
     def _word_embed(self, seqs):
         masks = seqs > 0
         outputs = self.word_embed(seqs)
         outputs = outputs * masks.unsqueeze(2).float()
         outputs = outputs.sum(1)
-        return outputs
+        return self._norm(outputs)
 
-    def _lstm_embed(self, seqs, is_first=True):
+    def _lstm_embed(self, seqs):
         lengths = (seqs > 0).sum(1)
         lengths[lengths == 0] = 1
-        if is_first:
-            embed = self.lstm_embed1(seqs)
-            outputs, _ = self.lstm1(embed)
-        else:
-            embed = self.lstm_embed2(seqs)
-            outputs, _ = self.lstm2(embed)
+        embed = self.lstm_embed(seqs)
+        outputs, _ = self.lstm(embed)
         outputs = [outputs[b, s - 1, :] for b, s in enumerate(lengths)]
         outputs = torch.cat(outputs).view(len(seqs), -1)
-        return outputs
+        return self._norm(outputs)
+
+    def _norm(self, x):
+        mean = torch.mean(x, dim=-1, keepdim=True)
+        std = torch.std(x, dim=-1, keepdim=True)
+        y = (x - mean) / (std + 1e-8)
+        return y
+
